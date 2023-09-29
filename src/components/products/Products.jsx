21bddc8a55455e9205733fc90019551b57c6dcf2
Achraf Mehanneche <achrafmehenneche.am@gmail.com>
2023-09-29 10:34:22 +0100

    feat: all Products page

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,108 @@
+import React from "react"
+
+import { useState, useEffect } from "react"
+import { SINGLE_PRODUCT } from "@/constants"
+
+function Products() {
+  // const product = {id, image, category, title, price};
+  const [fake, setFake] = useState([])
+  console.log(fake)
+  useEffect(() => {
+    fakestore()
+  }, [])
+
+  const fakestore = async () => {
+    const response = await fetch(SINGLE_PRODUCT)
+    // console.log(response)
+    const jsonData = await response.json()
+    // console.log(jsonData);
+    setFake(jsonData)
+  }
+  // fakestore();
+
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {fake.map((values) => {
+        return (
+          <>
+            <div className="w-full mx-auto max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+              <a className="block h-64" href="#">
+                <img
+                  className="p-8 rounded-t-lg max-h-64 max-w-64 w-auto mx-auto"
+                  src={values.image}
+                  alt=""
+                ></img>
+              </a>
+              <div className="px-5 pb-5 flex flex-col justify-between bg-white">
+                <a href="#">
+                  <h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-gray-900">
+                    {values.title}
+                  </h3>
+                </a>
+                <div class="flex items-center mt-2.5 mb-5">
+                  <svg
+                    class="w-5 h-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg
+                    class="w-5 h-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg
+                    class="w-5 h-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg
+                    class="w-5 h-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg
+                    class="w-5 h-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+                    {values.rating.rate}
+                  </span>
+                </div>
+                <div class="flex items-center justify-between">
+                  <span class="text-3xl font-bold text-gray-900 dark:text-gray-900">
+                    ${values.price}
+                  </span>
+                  {/* <a href="#"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
+                            to cart</a> */}
+                  <button className="bg-emerald-400 hover:bg-emerald-700 text-white font-bold py-2 px-2">
+                    {" "}
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Products
