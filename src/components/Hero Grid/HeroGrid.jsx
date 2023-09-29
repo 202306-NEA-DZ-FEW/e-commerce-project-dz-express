f45dbaae6feb0b988119b583e9c193d46eb92765
MerzoukFatima <merzoukfatimazahraa@gmail.com>
2023-09-29 17:36:38 +0100

    feat(herogrid): add link to products page and change style
    #15

--- src/components/Hero Grid/HeroGrid.jsx
+++ src/components/Hero Grid/HeroGrid.jsx
@@ -1,103 +1,65 @@
-import React from "react"
-import Image from "next/image"
+import React, { useEffect, useState } from "react"
+import { API } from "@/util/API"
+import { CATEGORIES } from "@/constants"
+import Link from "next/link"
+
 export default function HeroGrid() {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await API(CATEGORIES)
+        setCategories(data)
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="min-h-screen p-8 lg:p-16 grid grid-cols-1 grid-rows-5 gap-4 h-full w-full lg:grid-cols-7 lg:grid-rows-3">
-      <div
-        className="hero lg:col-span-7 bg-cover"
-        style={{ backgroundImage: `url('/images/shop_now.jpg')` }}
-      >
-        <div className="hero-overlay bg-opacity-40 relative"></div>
-        <div className="hero-content text-center text-neutral-content"></div>
+      <div className="hero lg:col-span-7 bg-mint">
+        <Link href={`/products/All`}>
+          <div className="hero-content text-center font-bold text-2xl font-mono text-black">
+            All
+          </div>
+        </Link>
       </div>
-      <div
-        className="hero col-span-5 row-start-2 bg-blue-200"
-        style={{ backgroundImage: `url('/images/category_electronics.jpg')` }}
-      >
-        <div className="hero-overlay bg-opacity-40 relative"></div>
-        <div className="hero-content text-center text-neutral-content"></div>
+      <div className="hero lg:col-span-5 lg:row-start-2 bg-tiffany-blue">
+        <Link href={`/products/${categories[0]}`}>
+          {" "}
+          <div className="hero-content text-center font-bold text-2xl font-mono text-black">
+            {categories[0]}
+          </div>
+        </Link>{" "}
       </div>
-      <div
-        className="hero col-span-2 col-start-6 row-start-2 bg-yellow-200"
-        style={{ backgroundImage: `url('/images/category_jewelery.jpg')` }}
-      >
-        <div className="hero-overlay bg-opacity-40 relative"></div>
-        <div className="hero-content text-center text-neutral-content"></div>
+
+      <div className="hero lg:col-span-2 lg:col-start-6 lg:row-start-2 bg-mint-green">
+        <Link href={`/products/${categories[1]}`}>
+          <div className="hero-content text-center font-bold text-2xl font-mono text-black">
+            {categories[1]}
+          </div>
+        </Link>{" "}
       </div>
-      <div
-        className="hero col-span-3 row-start-3 bg-red-200"
-        style={{ backgroundImage: `url('/images/category_women.jpg')` }}
-      >
-        <div className="hero-overlay bg-opacity-60 relative"></div>
-        <div className="hero-content text-center text-neutral-content"></div>
+
+      <div className="hero lg:col-span-3 lg:row-start-3 bg-mint-cream">
+        <Link href={`/products/${categories[2]}`}>
+          <div className="hero-content text-center font-bold text-2xl font-mono text-black">
+            {categories[2]}
+          </div>
+        </Link>{" "}
       </div>
-      <div
-        className="hero col-span-4 col-start-4 row-start-3 bg-purple-200"
-        style={{ backgroundImage: `url('/images/category_men.jpg')` }}
-      >
-        <div className="hero-overlay bg-opacity-60 relative"></div>
-        <div className="hero-content text-center text-neutral-content"></div>
+
+      <div className="hero lg:col-span-4 lg:col-start-4 lg:row-start-3 bg-zomp">
+        <Link href={`/products/${categories[3]}`}>
+          {" "}
+          <div className="hero-content text-center font-bold text-2xl font-mono text-black">
+            {categories[3]}
+          </div>
+        </Link>
       </div>
     </div>
   )
-  {
-    /*<div className="grid grid-cols-1 grid-rows-5 lg:grid-rows-9 gap-4 lg:h-128 lg:w-128 lg:grid-cols-7">
-  //   <div className="lg:col-span-7 lg:row-span-3 relative bg-green-200">
-  //   <div className="absolute bottom-0 left-0 right-0 top-auto h-auto w-auto bg-gradient-to-t from-green-200 via-transparent to-transparent opacity-0 transition rounded-lg duration-300 ease-in-out hover:opacity-70">
-  //      </div> 
-  //      <Image
-  //         className="lg:w-full lg:h-96 w-full h-full"
-  //         src="/images/shop_now.jpg"
-  //         alt="category_electronics"
-  //         width={1000}
-  //         height={1333}
-  // />
-  //   </div>
-
-  //     <div className="lg:col-span-5 lg:row-span-3 lg:row-start-4 relative">
-  //     <div className="absolute bottom-0 left-0 right-0 top-auto h-full w-full bg-gradient-to-t from-blue-200 via-transparent to-transparent opacity-0 transition rounded-lg duration-300 ease-in-out hover:opacity-70">
-  //      </div> 
-  //      <Image
-  //         className="lg:w-full lg:h-96 w-full h-full"
-  //         src="/images/category_electronics.jpg"
-  //         alt="category_electronics"
-  //         width={1000}
-  //         height={667}
-  //       />
-  //     </div>
-  //     <div className="lg:col-span-2 lg:row-span-3 lg:col-start-6 lg:row-start-4 relative">
-  //     <div className="absolute bottom-0 left-0 right-0 top-auto h-full w-full bg-gradient-to-t from-yellow-200 via-transparent to-transparent opacity-0 transition rounded-lg duration-300 ease-in-out hover:opacity-70">
-  //      </div> 
-  //       <Image
-  //         className="lg:h-96 lg:w-full w-full h-full"
-  //         src="/images/category_jewelery.jpg"
-  //         alt="Image"
-  //         width={1000}
-  //         height={800}
-  //       />
-  //     </div>
-  //     <div className="lg:col-span-3 lg:row-span-3 lg:row-start-7  relative">
-  //     <div className="absolute bottom-0 left-0 right-0 top-auto h-full w-full bg-gradient-to-t from-red-200 via-transparent to-transparent opacity-0 transition rounded-lg duration-300 ease-in-out hover:opacity-70">
-  //     </div> 
-  //       <Image
-  //         className="lg:w-full lg:h-96 w-full h-full"
-  //         src="/images/category_women.jpg"
-  //         alt="Image"
-  //         width={1000}
-  //         height={667}
-  //       />
-  //     </div>
-  //     <div className="lg:col-span-4 lg:row-span-3 lg:col-start-4 lg:row-start-7 relative">
-  //     <div className="absolute bottom-0 left-0 right-0 top-auto h-full w-full bg-gradient-to-t from-purple-200 via-transparent to-transparent opacity-0 transition rounded-lg duration-300 ease-in-out hover:opacity-70">
-  //      </div> 
-  //       <Image
-  //         className="lg:w-full lg:h-96 w-full h-full"
-  //         src="/images/category_men.jpg"
-  //         alt="Image"
-  //         width={1000}
-  //         height={667}
-  //       />
-  //     </div>
-  //   </div>*/
-  }
 }
