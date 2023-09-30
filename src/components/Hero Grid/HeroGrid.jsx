b99a79c8c403f06a2e8cc7e5cae40cf93fd28ad9
MerzoukFatima <merzoukfatimazahraa@gmail.com>
2023-10-01 00:01:47 +0100

    refactor(hero grid): adjust herogrid and index

--- src/components/Hero Grid/HeroGrid.jsx
+++ src/components/Hero Grid/HeroGrid.jsx
@@ -3,9 +3,7 @@ import { API } from "@/util/API"
 import { CATEGORIES } from "@/constants"
 import Link from "next/link"
 
-export default function HeroGrid() {
-  const [categories, setCategories] = useState([])
-
+export default function HeroGrid(categories) {
   useEffect(() => {
     async function fetchData() {
       try {
@@ -21,14 +19,14 @@ export default function HeroGrid() {
   return (
     <div className="min-h-screen p-8 lg:p-16 grid grid-cols-1 grid-rows-5 gap-4 h-full w-full lg:grid-cols-7 lg:grid-rows-3">
       <div className="hero lg:col-span-7 bg-mint">
-        <Link href={`/products/All`}>
+        <Link href={`/products`}>
           <div className="hero-content text-center font-bold text-2xl font-mono text-black">
             All
           </div>
         </Link>
       </div>
       <div className="hero lg:col-span-5 lg:row-start-2 bg-tiffany-blue">
-        <Link href={`/products/${categories[0]}`}>
+        <Link href={`/products}`}>
           {" "}
           <div className="hero-content text-center font-bold text-2xl font-mono text-black">
             {categories[0]}
@@ -37,7 +35,7 @@ export default function HeroGrid() {
       </div>
 
       <div className="hero lg:col-span-2 lg:col-start-6 lg:row-start-2 bg-mint-green">
-        <Link href={`/products/${categories[1]}`}>
+        <Link href={`/products}`}>
           <div className="hero-content text-center font-bold text-2xl font-mono text-black">
             {categories[1]}
           </div>
@@ -45,7 +43,7 @@ export default function HeroGrid() {
       </div>
 
       <div className="hero lg:col-span-3 lg:row-start-3 bg-mint-cream">
-        <Link href={`/products/${categories[2]}`}>
+        <Link href={`/products}`}>
           <div className="hero-content text-center font-bold text-2xl font-mono text-black">
             {categories[2]}
           </div>
@@ -53,7 +51,7 @@ export default function HeroGrid() {
       </div>
 
       <div className="hero lg:col-span-4 lg:col-start-4 lg:row-start-3 bg-zomp">
-        <Link href={`/products/${categories[3]}`}>
+        <Link href={`/products}`}>
           {" "}
           <div className="hero-content text-center font-bold text-2xl font-mono text-black">
             {categories[3]}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,42 @@
 import { API } from "@/util/API"
-import { SINGLE_PRODUCT } from "@/constants"
+import { ALL_PRODUCTS, CATEGORIES, SINGLE_PRODUCT } from "@/constants"
 import ProductCard from "@/components/ProductCard/ProductCard"
 import HeroGrid from "@/components/Hero Grid/HeroGrid"
 
-export default function Home({ products }) {
+export default function Home({ categories, categoryObjects }) {
   return (
     <main className="flex flex-col min-h-screen">
-      <HeroGrid />
-      <ProductCard
-        id={products.id}
-        title={products.title}
-        price={products.price}
-        description={products.description}
-        category={products.category}
-        image={products.image}
-        rating={products.rating.rate}
-        count={products.rating.count}
-      />
+      <HeroGrid {...categories} />
+      <div className="grid grid-cols-4 gap-4">
+        {categoryObjects.map((categoryObj) => (
+          <div key={categoryObj.category} className="col-span-4">
+            <h2 className="text-xl font-bold">{categoryObj.category}</h2>
+            <div className="grid grid-cols-3 gap-4"></div>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
 
 export async function getStaticProps() {
-  const data = await API(SINGLE_PRODUCT)
+  const products = await API(ALL_PRODUCTS)
+  const categories = await API(CATEGORIES)
+  const categoryObjects = categories.map((category) => {
+    const matchingProducts = products.filter(
+      (product) => product.category === category,
+    )
+    const limitedProducts = matchingProducts.slice(0, 3) // Get the first 3 matching products
+
+    return {
+      category: category,
+      products: limitedProducts,
+    }
+  })
   return {
     props: {
-      products: data,
+      categories: categories,
+      categoryObjects: categoryObjects,
     },
   }
 }
