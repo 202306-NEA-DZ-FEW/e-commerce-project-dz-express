4c58f1d95eebb06b9932b6a565d3163c4b4d89ca
EmyBel <belaidimane@gmail.com>
2023-09-30 09:31:04 +0100

    fix(layout file bug): delete Layout file

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -71,9 +71,6 @@ function ProductCard({ title, price, image, category, rating, count }) {
               {/* Replace this with Price and communt it to see the card */}$
               {price}
             </span>
-            {/* <a href="#"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-                                to cart</a> */}
             <button className="bg-emerald-400 hover:bg-emerald-700 text-white font-bold py-2 px-2">
               {" "}
               Add to Cart
