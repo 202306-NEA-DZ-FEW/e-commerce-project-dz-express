4884178a018a2d0277c2d47899a18cd455059878
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2023-09-30 10:59:09 +0100

    refactor(project): adding manually some missing code

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Nav from "./Nav/Nav"
+import Footer from "./footer/Footer"
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Nav />
+      <main> {children} </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
 import { API } from "@/util/API"
-import { ALL_CART } from "@/constants"
+import { SINGLE_PRODUCT } from "@/constants"
+import ProductCard from "@/components/ProductCard/ProductCard"
+import HeroGrid from "@/components/Hero Grid/HeroGrid"
 
 export default function Home({ products }) {
-  return <main className="flex flex-col min-h-screen"></main>
+  return (
+    <main className="flex flex-col min-h-screen">
+      <HeroGrid />
+      <ProductCard
+        id={products.id}
+        title={products.title}
+        price={products.price}
+        description={products.description}
+        category={products.category}
+        image={products.image}
+        rating={products.rating.rate}
+        count={products.rating.count}
+      />
+    </main>
+  )
 }
 
-// export async function getStaticProps() {
-//   const data = await API(ALL_CART)
-//   return {
-//     props: {
-//       products: data,
-//     },
-//   }
-// }
+export async function getStaticProps() {
+  const data = await API(SINGLE_PRODUCT)
+  return {
+    props: {
+      products: data,
+    },
+  }
+}
